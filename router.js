eaa418fac0d4b072fbeb64c65647d1a64fe5426a
Jennifer Bolanos <jbolanos707@gmail.com>
2015-06-12 10:13:10 -0700

    allow user to click a question and be routed to that questions page

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 Board.Router.map(function() {
   this.resource('questions', {path: "/"});
   this.resource('new-question');
+  this.resource('question', {path: 'questions/:question_id'})
   // this.resource('recent-answers');
   // this.resource('question', {path: 'questions/:question_id'}, function() {
   //   this.resource('new-answer');
